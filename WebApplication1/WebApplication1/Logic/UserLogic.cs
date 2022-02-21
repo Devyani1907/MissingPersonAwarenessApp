10edf385091c79af5e879e38820b4286ae802f3f
Devyani1907 <devyani.r.pandey@gmail.com>
2022-02-21 04:10:03 +0000

    FrontEnd > Registration BackEnd > Login

---
+++ WebApplication1/WebApplication1/Logic/UserLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using WebApplication1.Data;
+
+namespace WebApplication1.Logic
+{
+    public class UserLogic
+    {
+        public UserModel ValidUser(UserModel model)
+        {
+            UserModel userModel = new UserModel();
+
+            using (MissingPersonEntities entities = new MissingPersonEntities())
+            {
+
+                // Get User by Email
+                var userDetail = from user in entities.Users
+                                 where user.Email == model.Email
+                                 select new UserModel
+                                 {
+                                     Email = user.Email,
+                                     Password = user.Password
+                                 };
+            }
+            return userModel;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string CreatedDatetime { get; set; }
+        public string UpdatedDatetime { get; set; }
+    }
+}
